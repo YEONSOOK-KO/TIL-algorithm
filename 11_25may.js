2e313b78a7d8e40259c788abb4beb1734715e546
YEONSOOK-KO <dustnr0121@gmail.com>
2023-05-25 16:36:02 +0900

    Modify: 11_25may.js

---
+++ 11_25may.js
@@ -0,0 +1,16 @@
+// programmers question URL : https://school.programmers.co.kr/learn/courses/30/lessons/181945
+// 문자열 돌리기
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line
+}).on('close',function(){
+    [...input].forEach(a => console.log(a))
+});
