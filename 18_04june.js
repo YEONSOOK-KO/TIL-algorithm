7ad0f827f6920f679c7b8c7b606023a62b456b01
YEONSOOK-KO <dustnr0121@gmail.com>
2023-06-04 12:13:26 +0900

    18_04june.js

---
+++ 18_04june.js
@@ -0,0 +1,8 @@
+// programmers question URL : https://school.programmers.co.kr/learn/courses/30/lessons/181937
+// n의 배수
+
+function solution(num, n) {
+    return num % n === 0 ? 1 : 0
+}
+
+// 삼항연산자가 이제 편안해졌당ㅋ 굿
