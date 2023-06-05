a681e1c5cd2b955e8c544d42ace1258154abc0ba
YEONSOOK-KO <dustnr0121@gmail.com>
2023-06-05 17:03:08 +0900

    Add: 19_05june.js

---
+++ 19_05june.js
@@ -0,0 +1,6 @@
+// programmer question URL : https://school.programmers.co.kr/learn/courses/30/lessons/181936
+// 공배수
+
+function solution(number, n, m) {
+    return number % n === 0 && number % m === 0 ? 1 : 0;
+}
