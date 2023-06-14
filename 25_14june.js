62e027e49292bc482522bbc27ce9690fb7fb5cde
YEONSOOK-KO <dustnr0121@gmail.com>
2023-06-14 17:49:00 +0900

    Add: 25_14june.js

---
+++ 25_14june.js
@@ -0,0 +1,11 @@
+// programmers question URL : https://school.programmers.co.kr/learn/courses/30/lessons/181920
+// 카운트 업
+
+function solution(start, end) {
+    let result = []
+    
+    for (let i = start; i <= end; i++) {
+        result.push(i)
+    }
+    return result;
+}
